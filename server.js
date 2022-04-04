f01ae78142db0a8f2be3bc7c78876672d8b724f4
David Křístek <david.kristek05@gmail.com>
2022-04-04 18:38:54 +0200

    sendGrid email

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("tiny"))
+app.use(morgan("tiny"));
 app.get("/", (req, res) => {
   res.send("Hello");
 });
@@ -42,5 +42,5 @@ import task from "./routes/task.js";
 app.use("/auth", auth);
 app.use("/task", task);
 app.get("/errorstatus", (req, res) => {
-  res.status(500).json({message: "Ahojda"})
-})
+  res.status(500).json({ message: "Ahojda" });
+});
